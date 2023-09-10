7b2675079409bf1f1e2e7059fa98bd37a75f428c
Frank Zappulla <frank@codeocean.com>
2023-09-10 21:40:32 +0000

    Edited cellranger.sh, run, Dockerfile, postInstall

--- code/cellranger.sh
+++ code/cellranger.sh
@@ -2,4 +2,7 @@
 
 cd /results
 
-cellranger count --id my_run --fastqs /data/L8TX_171026_01_A05 --sample L8TX_171026_01_A05 --transcriptome /data/cellranger_mouse_reference/refdata-gex-mm10-2020-A
+cellranger count --id=incf_run \
+--fastqs=/data/u19_zeng_fastq \
+--sample=NW_TX0005-23 \
+--transcriptome=/data/cellranger_mouse_reference/refdata-gex-mm10-2020-A
